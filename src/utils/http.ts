33bd69c77d4f019366d984104c992b0e60172953
plongtran239 <plongtran239@gmail.com>
2023-01-27 02:30:32 +0700

    Handle simple message with interceptor

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,7 @@
-import axios, { type AxiosInstance } from 'axios';
+import axios, { AxiosError, type AxiosInstance } from 'axios';
+import { toast } from 'react-toastify';
+
+import HttpStatusCode from 'src/constants/httpStatusCode.enum';
 
 class Http {
     instance: AxiosInstance;
@@ -11,6 +14,29 @@ class Http {
                 'Content-Type': 'application/json'
             }
         });
+        this.instance.interceptors.response.use(
+            function (response) {
+                return response;
+            },
+            function (error: AxiosError) {
+                if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    const data: any | undefined = error.response?.data;
+                    const message = data.message || error.message;
+                    toast.error(message, {
+                        position: 'top-right',
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: 'light'
+                    });
+                }
+                return Promise.reject(error);
+            }
+        );
     }
 }
 
