3f37067b46adc661905ba6fb9970f9076f3f8ed9
plongtran239 <plongtran239@gmail.com>
2023-01-28 13:47:08 +0700

    Code UI MainLayout/Header

--- src/useRouteElements.tsx
+++ src/useRouteElements.tsx
@@ -4,12 +4,17 @@ import ProductList from './pages/ProductList';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import RegisterLayout from './layouts/RegisterLayout';
+import MainLayout from './layouts/MainLayout';
 
 export default function useRouteElements() {
     const routeElements = useRoutes([
         {
             path: '/',
-            element: <ProductList />
+            element: (
+                <MainLayout>
+                    <ProductList />
+                </MainLayout>
+            )
         },
         {
             path: 'login',
