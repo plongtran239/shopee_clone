910496cfff97929ec5fe1e8efcd430b19b23c739
plongtran239 <plongtran239@gmail.com>
2023-01-28 14:54:02 +0700

    Add popover with floating UI and framer motion

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,39 @@
+import { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FloatingPortal, useFloating, arrow, shift, offset } from '@floating-ui/react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+    const arrowRef = useRef<HTMLElement>(null);
+
+    const { x, y, strategy, refs, middlewareData } = useFloating({
+        middleware: [
+            offset(0),
+            shift(),
+            arrow({
+                element: arrowRef
+            })
+        ]
+    });
+
+    const showPopover = () => {
+        setIsOpen(true);
+    };
+    const hidePopover = () => {
+        setIsOpen(false);
+    };
+
     return (
         <div className='bg-[linear-gradient(-180deg,#f53d2d,#f63)] pb-5 pt-2 text-white'>
             <div className='container'>
                 <div className='flex justify-end'>
-                    <div className='flex cursor-pointer items-center py-1 hover:text-gray-300'>
+                    <div
+                        className='flex cursor-pointer items-center py-1 hover:text-gray-300'
+                        ref={refs.setReference}
+                        onMouseEnter={showPopover}
+                        onMouseLeave={hidePopover}
+                    >
                         <svg
                             xmlns='http://www.w3.org/2000/svg'
                             fill='none'
@@ -31,7 +59,41 @@ export default function Header() {
                         >
                             <path strokeLinecap='round' strokeLinejoin='round' d='M19.5 8.25l-7.5 7.5-7.5-7.5' />
                         </svg>
+
+                        <FloatingPortal>
+                            <AnimatePresence>
+                                {isOpen && (
+                                    <motion.div
+                                        ref={refs.setFloating}
+                                        style={{
+                                            position: strategy,
+                                            top: y ?? 0,
+                                            left: x ?? 0,
+                                            width: 'max-content',
+                                            transformOrigin: `${middlewareData.arrow?.x}px top`
+                                        }}
+                                        initial={{ opacity: 0, transform: 'scale(0)' }}
+                                        animate={{ opacity: 1, transform: 'scale(1)' }}
+                                        exit={{ opacity: 0, transform: 'scale(0)' }}
+                                        transition={{ duration: 0.2 }}
+                                    >
+                                        <span
+                                            ref={arrowRef}
+                                            className='absolute z-10 translate-y-[-82%] border-[11px] border-x-transparent border-t-transparent border-b-white'
+                                            style={{ left: middlewareData.arrow?.x, top: middlewareData.arrow?.y }}
+                                        />
+                                        <div className='border-grey-200 relative rounded-md border bg-white shadow-md'>
+                                            <div className='flex flex-col py-2 px-3'>
+                                                <button className='py-2 px-3 hover:text-orange'>Tiếng Việt</button>
+                                                <button className='py-2 px-3 hover:text-orange'>English</button>
+                                            </div>
+                                        </div>
+                                    </motion.div>
+                                )}
+                            </AnimatePresence>
+                        </FloatingPortal>
                     </div>
+
                     <div className='ml-6 flex cursor-pointer items-center py-1 hover:text-gray-300'>
                         <div className='mr-2 h-6 w-6 flex-shrink-0'>
                             <img
