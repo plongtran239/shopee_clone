55ef7f20c13f8e0ad6d99feecd97fe49eb2ef175
plongtran239 <plongtran239@gmail.com>
2023-01-30 16:24:54 +0700

    Add component ProductRating

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 
 import { Product as ProductType } from 'src/types/product.type';
 import { formatNumberToSocial, formatCurrency } from 'src/utils/utils';
+import ProductRating from 'src/components/ProductRating';
 
 interface ProductProps {
     product: ProductType;
@@ -31,40 +32,7 @@ export default function Product({ product }: ProductProps) {
                         </div>
                     </div>
                     <div className='mt-3 flex items-center justify-end'>
-                        <div className='flex items-center'>
-                            <div className='relative'>
-                                <div className='absolute top-0 left-0 h-full overflow-hidden' style={{ width: '50%' }}>
-                                    <svg
-                                        enableBackground='new 0 0 15 15'
-                                        viewBox='0 0 15 15'
-                                        x={0}
-                                        y={0}
-                                        className='h-3 w-3 fill-yellow-300 text-yellow-300'
-                                    >
-                                        <polygon
-                                            points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
-                                            strokeLinecap='round'
-                                            strokeLinejoin='round'
-                                            strokeMiterlimit={10}
-                                        />
-                                    </svg>
-                                </div>
-                                <svg
-                                    enableBackground='new 0 0 15 15'
-                                    viewBox='0 0 15 15'
-                                    x={0}
-                                    y={0}
-                                    className='h-3 w-3 fill-current text-gray-300'
-                                >
-                                    <polygon
-                                        points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
-                                        strokeLinecap='round'
-                                        strokeLinejoin='round'
-                                        strokeMiterlimit={10}
-                                    />
-                                </svg>
-                            </div>
-                        </div>
+                        <ProductRating rating={product.rating} />
                         <div className='ml-2 text-sm'>
                             <span>{formatNumberToSocial(product.sold)}</span>
                             <span className='ml-1'>Đã bán</span>
