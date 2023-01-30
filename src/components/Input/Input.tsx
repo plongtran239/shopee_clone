8cbdc155e341e21af64d24a8138ee3b4cbb94392
plongtran239 <plongtran239@gmail.com>
2023-01-30 11:36:34 +0700

    Code UI AsideFilter

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,13 +1,11 @@
+import { InputHTMLAttributes } from 'react';
 import type { RegisterOptions, UseFormRegister } from 'react-hook-form';
 
-interface Props {
-    className?: string;
-    type: React.HTMLInputTypeAttribute;
-    placeholder?: string;
-    autoComplete?: string;
+interface Props extends InputHTMLAttributes<HTMLElement> {
+    classNameInput?: string;
+    classNameError?: string;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    register: UseFormRegister<any>;
-    name: string;
+    register?: UseFormRegister<any>;
     rules?: RegisterOptions;
     errorMessage?: string;
 }
@@ -20,18 +18,22 @@ export default function Input({
     register,
     name,
     rules,
-    errorMessage
+    errorMessage,
+    classNameInput = 'p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm',
+    classNameError = 'mt-1 text-red-600 min-h-[1.25rem] text-sm'
 }: Props) {
+    const registerResult = register && name ? register(name, rules) : {};
+
     return (
         <div className={className}>
             <input
                 type={type}
-                className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
+                className={classNameInput}
                 placeholder={placeholder}
                 autoComplete={autoComplete}
-                {...register(name, rules)}
+                {...registerResult}
             />
-            <div className='mt-1 min-h-[1.25rem] text-sm text-red-600'>{errorMessage}</div>
+            <div className={classNameError}>{errorMessage}</div>
         </div>
     );
 }
