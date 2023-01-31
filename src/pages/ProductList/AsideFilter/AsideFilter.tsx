d28d6e04169b70377f910ac9ae85351d997b8624
plongtran239 <plongtran239@gmail.com>
2023-01-31 13:34:56 +0700

    Code logic category for AsideFilter

--- src/pages/ProductList/AsideFilter/AsideFilter.tsx
+++ src/pages/ProductList/AsideFilter/AsideFilter.tsx
@@ -1,13 +1,28 @@
-import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import { createSearchParams, Link } from 'react-router-dom';
 
 import Button from 'src/components/Button';
 import Input from 'src/components/Input';
 import paths from 'src/constants/paths';
+import { Category } from 'src/types/category.type';
+import { QueryConfig } from '../ProductList';
 
-export default function AsideFilter() {
+interface Props {
+    queryConfig: QueryConfig;
+    categories: Category[];
+}
+
+export default function AsideFilter({ categories, queryConfig }: Props) {
+    const { category } = queryConfig;
+
     return (
         <div className='py-4'>
-            <Link to={paths.home} className='flex items-center font-bold'>
+            <Link
+                to={paths.home}
+                className={classNames('flex items-center font-bold', {
+                    'text-orange': !category
+                })}
+            >
                 <svg viewBox='0 0 12 10' className='mr-3 h-4 w-3 fill-current'>
                     <g fillRule='evenodd' stroke='none' strokeWidth={1}>
                         <g transform='translate(-373 -208)'>
@@ -25,19 +40,32 @@ export default function AsideFilter() {
             </Link>
             <div className='my-4 h-[1px] bg-gray-300' />
             <ul>
-                <li className='py-2 pl-2'>
-                    <Link to={paths.home} className='relative px-2 font-semibold text-orange'>
-                        <svg viewBox='0 0 4 7' className='absolute top-1 left-[-10px] h-2 w-2 fill-orange'>
-                            <polygon points='4 3.5 0 0 0 7' />
-                        </svg>
-                        Thời trang nam
-                    </Link>
-                </li>
-                <li className='py-2 pl-2'>
-                    <Link to={paths.home} className='relative px-2 '>
-                        Điện thoại
-                    </Link>
-                </li>
+                {categories.map((categoryItem) => {
+                    const isActive = category === categoryItem._id;
+                    return (
+                        <li key={categoryItem._id} className='py-2 pl-2'>
+                            <Link
+                                to={{
+                                    pathname: paths.home,
+                                    search: createSearchParams({
+                                        ...queryConfig,
+                                        category: categoryItem._id
+                                    }).toString()
+                                }}
+                                className={classNames('relative px-2 ', {
+                                    'font-semibold text-orange': ''
+                                })}
+                            >
+                                {isActive && (
+                                    <svg viewBox='0 0 4 7' className='absolute top-1 left-[-10px] h-2 w-2 fill-orange'>
+                                        <polygon points='4 3.5 0 0 0 7' />
+                                    </svg>
+                                )}
+                                {categoryItem.name}
+                            </Link>
+                        </li>
+                    );
+                })}
             </ul>
             <Link to={paths.home} className='mt-4 flex items-center font-bold uppercase'>
                 <svg
--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { omitBy, isUndefined } from 'lodash';
 
 import productApi from 'src/apis/product.api';
+import categoryApi from 'src/apis/category.api';
 import useQueryParams from 'src/hooks/useQueryParams';
 import AsideFilter from './AsideFilter';
 import Product from './Product';
@@ -15,6 +16,7 @@ export type QueryConfig = {
 
 export default function ProductList() {
     const queryParams: QueryConfig = useQueryParams();
+
     const queryConfig: QueryConfig = omitBy(
         {
             page: queryParams.page || '1',
@@ -25,11 +27,13 @@ export default function ProductList() {
             order: queryParams.order,
             price_max: queryParams.price_max,
             price_min: queryParams.price_min,
-            rating_filter: queryParams.rating_filter
+            rating_filter: queryParams.rating_filter,
+            category: queryParams.category
         },
         isUndefined
     );
-    const { data } = useQuery({
+
+    const { data: productsData } = useQuery({
         queryKey: ['products', queryParams],
         queryFn: () => {
             return productApi.getProducts(queryConfig as ProductListConfig);
@@ -37,24 +41,37 @@ export default function ProductList() {
         keepPreviousData: true
     });
 
+    const { data: categoriesData } = useQuery({
+        queryKey: ['categories'],
+        queryFn: () => {
+            return categoryApi.getCategories();
+        }
+    });
+
     return (
         <div className='bg-gray-200 py-6'>
             <div className='container'>
-                {data && (
+                {productsData && (
                     <div className='grid grid-cols-12 gap-6'>
                         <div className='col-span-3'>
-                            <AsideFilter />
+                            <AsideFilter categories={categoriesData?.data.data || []} queryConfig={queryConfig} />
                         </div>
                         <div className='col-span-9'>
-                            <SortProductList queryConfig={queryConfig} pageSize={data.data.data.pagination.page_size} />
+                            <SortProductList
+                                queryConfig={queryConfig}
+                                pageSize={productsData.data.data.pagination.page_size}
+                            />
                             <div className='mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-                                {data.data.data.products.map((product) => (
+                                {productsData.data.data.products.map((product) => (
                                     <div className='col-span-1' key={product._id}>
                                         <Product product={product} />
                                     </div>
                                 ))}
                             </div>
-                            <Pagination queryConfig={queryConfig} pageSize={data.data.data.pagination.page_size} />
+                            <Pagination
+                                queryConfig={queryConfig}
+                                pageSize={productsData.data.data.pagination.page_size}
+                            />
                         </div>
                     </div>
                 )}
