facdae392c20ed13dc2ddf3ed8c63d2517912f9d
plongtran239 <plongtran239@gmail.com>
2023-02-01 15:24:18 +0700

    Code logic zoom image

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import DOMPurify from 'dompurify';
@@ -6,7 +7,6 @@ import productApi from 'src/apis/product.api';
 import ProductRating from 'src/components/ProductRating';
 import { formatCurrency, formatNumberToSocialStyle, rateSale } from 'src/utils/utils';
 import InputNumber from 'src/components/InputNumber';
-import { useEffect, useMemo, useState } from 'react';
 import { Product } from 'src/types/product.type';
 
 export default function ProductDetail() {
@@ -19,6 +19,7 @@ export default function ProductDetail() {
     const [currentIndexImages, setCurrentIndexImages] = useState([0, 5]);
     const [activeImage, setActiveImage] = useState('');
     const product = ProductDetailData?.data.data;
+    const imageRef = useRef<HTMLImageElement>(null);
     const currentImages = useMemo(
         () => (product ? product?.images.slice(...currentIndexImages) : []),
         [product, currentIndexImages]
@@ -46,6 +47,31 @@ export default function ProductDetail() {
         setActiveImage(img);
     };
 
+    const handleZoom = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        const rect = event.currentTarget.getBoundingClientRect();
+        const image = imageRef.current as HTMLImageElement;
+        const { naturalHeight, naturalWidth } = image;
+        // Cách 1: Lấy offsetX, offsetY đơn giản khi chúng ta đã xử lý được bubble event
+        // Dùng css pointer-events-none
+        // const { offsetX, offsetY } = event.nativeEvent;
+
+        // Cách 2: Lấy offsetX, offsetY khi chúng ta không xử lý được bubble event
+        const offsetX = event.pageX - (rect.x + window.scrollX);
+        const offsetY = event.pageY - (rect.y + window.scrollY);
+
+        const top = offsetY * (1 - naturalHeight / rect.height);
+        const left = offsetX * (1 - naturalWidth / rect.width);
+        image.style.width = naturalWidth + 'px';
+        image.style.height = naturalHeight + 'px';
+        image.style.maxWidth = 'unset';
+        image.style.top = top + 'px';
+        image.style.left = left + 'px';
+    };
+
+    const handleRemoveZoom = () => {
+        imageRef.current?.removeAttribute('style');
+    };
+
     if (!product) {
         return null;
     }
@@ -56,11 +82,16 @@ export default function ProductDetail() {
                 <div className='bg-white p-4 shadow'>
                     <div className='grid grid-cols-12 gap-9'>
                         <div className='col-span-5'>
-                            <div className='relative w-full pt-[100%] shadow'>
+                            <div
+                                className='relative w-full cursor-zoom-in overflow-hidden pt-[100%] shadow'
+                                onMouseMove={handleZoom}
+                                onMouseLeave={handleRemoveZoom}
+                            >
                                 <img
                                     src={activeImage}
                                     alt={product.name}
                                     className='absolute top-0 left-0 h-full w-full bg-white object-cover'
+                                    ref={imageRef}
                                 />
                             </div>
                             <div className='relative mt-4 grid grid-cols-5 gap-1'>
